0e02f73985fde068630ab93eff260cc3874295df
esbenscriver <esbenscriver@gmail.com>
2025-09-17 10:56:16 +0200

    add stochasticity, print table of estimates

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ from jax import random
 # import solver for one-to-one matching model
 from estimate_matching_model.matching_model import MatchingModel, ObservedData
 
+from tabulate import tabulate
+
 # Increase precision to 64 bit
 jax.config.update("jax_enable_x64", True)
 
-# Set number of types of agents on both sides of the market
-types_X, types_Y = 40, 60
-
+types_X, types_Y = 400, 200
 number_of_parameters_X, number_of_parameters_Y = 2, 3
 
-# Simulate choice-specific utilities
+# Simulate covariates of the agents' utility function
 covariates_X = -random.uniform(
     key=random.PRNGKey(111), shape=(types_X, types_Y, number_of_parameters_X)
 )
@@ -26,18 +26,16 @@ covariates_Y = random.uniform(
     key=random.PRNGKey(112), shape=(types_X, types_Y, number_of_parameters_Y)
 )
 
-# Simulate choice-specific utilities
+# Simulate parameters of the agents' utility function
 parameters = random.uniform(
     key=random.PRNGKey(113), shape=(number_of_parameters_X + number_of_parameters_Y,)
 )
 
-# Simulate distribution of agents
-marginal_distribution_X = random.uniform(key=random.PRNGKey(115), shape=(types_X, 1))
-marginal_distribution_Y = random.uniform(key=random.PRNGKey(116), shape=(1, types_Y))
+# Simulate marginal distribution of agents
+marginal_distribution_X = random.uniform(key=random.PRNGKey(114), shape=(types_X, 1))
+marginal_distribution_Y = random.uniform(key=random.PRNGKey(115), shape=(1, types_Y))
 
-print("-----------------------------------------------------------------------")
-print("Solve a matching model with logit demand:")
-
+# Solve a matching model with logit demand
 model = MatchingModel(
     covariates_X=covariates_X,
     covariates_Y=covariates_Y,
@@ -47,26 +45,32 @@ model = MatchingModel(
 
 utility_X, utility_Y = model.Utilities_of_agents(params=parameters)
 
-transfer = model.solve(utility_X=utility_X, utility_Y=utility_Y, verbose=True)
+transfer = model.solve(utility_X=utility_X, utility_Y=utility_Y)
 
 pX_xy, pX_x0 = model.ChoiceProbabilities_X(transfer, utility_X)
 pY_xy, pY_0y = model.ChoiceProbabilities_Y(transfer, utility_Y)
 
+# Simulate data
+sigma = 1.0
+measurement_errors = sigma * random.normal(random.PRNGKey(211), shape=transfer.shape)
+observed_transfer = transfer + measurement_errors
+
 data = ObservedData(
-    transfer=transfer,
+    transfer=observed_transfer,
     matched=model.marginal_distribution_X * pX_xy,
     unmatched_X=model.marginal_distribution_X * pX_x0,
     unmatched_Y=model.marginal_distribution_Y * pY_0y,
 )
 
-assert jnp.allclose(data.matched, model.marginal_distribution_Y * pY_xy), (
-    "demand do not match"
-)
-
 guess = jnp.zeros_like(parameters)
 
-parameter_estimates = model.fit(guess, data)
+# Estimate parameters by maximum likelihood estimation
+parameter_estimates = model.fit(guess, data, verbose=True)
 
-assert jnp.allclose(parameter_estimates, parameters), (
-    "true parameters and estimated parameters do no match"
-)
+# Combine into rows
+table = list(zip(parameters, parameter_estimates))
+
+# Print with headers
+print(tabulate(table, headers=["True parameters", "Estimated parameters"], tablefmt="grid"))
+
+
