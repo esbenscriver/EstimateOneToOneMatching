4f91c5580c11cd590b49cebbc6585f673ee5110a
esbenscriver <esbenscriver@gmail.com>
2025-09-19 13:46:21 +0200

    use jax.Array for type annotation instead of jnp.ndarray

--- src/estimate_matching_model/matching_model.py
+++ src/estimate_matching_model/matching_model.py
@@ -5,7 +5,9 @@ Reference:
 Esben Scriver Andersen, Note on solving one-to-one matching models with linear transferable utility, 2025 (https://arxiv.org/pdf/2409.05518)
 """
 
+import jax
 import jax.numpy as jnp
+from jax import Array
 
 # import simple_pytree (used to store variables)
 from simple_pytree import Pytree, dataclass
@@ -24,16 +26,16 @@ class ObservedData(Pytree, mutable=False):
     """Observed data used for maximum likelihood estimation
 
     Attributes:
-        transfer (jnp.ndarray): observed transfers between matched agents
-        matched (jnp.ndarray): observed numbers of matched agents
-        unmatched_X (jnp.ndarray): observed numbers of unmatched agents of type X
-        unmatched_Y (jnp.ndarray): observed numbers of unmatched agents of type Y
+        transfer (Array): observed transfers between matched agents
+        matched (Array): observed numbers of matched agents
+        unmatched_X (Array): observed numbers of unmatched agents of type X
+        unmatched_Y (Array): observed numbers of unmatched agents of type Y
     """
 
-    transfer: jnp.ndarray
-    matched: jnp.ndarray
-    unmatched_X: jnp.ndarray
-    unmatched_Y: jnp.ndarray
+    transfer: Array
+    matched: Array
+    unmatched_X: Array
+    unmatched_Y: Array
 
 
 @dataclass
@@ -41,31 +43,29 @@ class MatchingModel(Pytree, mutable=False):
     """Matching model
 
     Attributes:
-        covariates_X (jnp.ndarray): covariates of utility function of agents of type X
-        covariates_Y (jnp.ndarray): covariates of utility function of agents of type Y
-        marginal_distribution_X (jnp.ndarray): marginal distribution of agents of type X
-        marginal_distribution_Y (jnp.ndarray): marginal distribution of agents of type Y
+        covariates_X (Array): covariates of utility function of agents of type X
+        covariates_Y (Array): covariates of utility function of agents of type Y
+        marginal_distribution_X (Array): marginal distribution of agents of type X
+        marginal_distribution_Y (Array): marginal distribution of agents of type Y
     """
 
-    covariates_X: jnp.ndarray
-    covariates_Y: jnp.ndarray
+    covariates_X: Array
+    covariates_Y: Array
 
-    marginal_distribution_X: jnp.ndarray
-    marginal_distribution_Y: jnp.ndarray
+    marginal_distribution_X: Array
+    marginal_distribution_Y: Array
 
-    def ChoiceProbabilities(
-        self, v: jnp.ndarray, axis: int
-    ) -> tuple[jnp.ndarray, jnp.ndarray]:
+    def ChoiceProbabilities(self, v: Array, axis: int) -> tuple[Array, Array]:
         """Compute the logit choice probabilities for inside and outside options
 
         Args:
-            v (jnp.ndarray): choice-specific payoffs
+            v (Array): choice-specific payoffs
             axis (int): axis that describes the choice set
 
         Returns:
-        P_inside (jnp.ndarray):
+        P_inside (Array):
             choice probabilities of inside options.
-        P_outside (jnp.ndarray):
+        P_outside (Array):
             choice probabilities of outside option.
         """
         # v_max = jnp.max(v, axis=axis, keepdims=True)
@@ -77,72 +77,70 @@ class MatchingModel(Pytree, mutable=False):
         denominator = 1 + jnp.sum(nominator, axis=axis, keepdims=True)
         return nominator / denominator, 1 / denominator
 
-    def Utility(self, covariates: jnp.ndarray, parameters: jnp.ndarray) -> jnp.ndarray:
+    def Utility(self, covariates: Array, parameters: Array) -> Array:
         """Computes match-specific utilities
 
         Args:
-            covariates (jnp.ndarray): covariates of utility function
-            parameters (jnp.ndarray): parameters of utility function
+            covariates (Array): covariates of utility function
+            parameters (Array): parameters of utility function
 
         Returns:
-            demand (jnp.ndarray): match-specific utilities
+            demand (Array): match-specific utilities
         """
         return jnp.einsum("ijk, k -> ij", covariates, parameters)
 
-    def ChoiceProbabilities_X(
-        self, transfer: jnp.ndarray, utility_X: jnp.ndarray
-    ) -> tuple[jnp.ndarray, jnp.ndarray]:
+    def ChoiceProbabilities_X(self, transfer: Array, utility_X: Array) -> tuple[Array, Array]:
         """Computes choice probabilities of agents of type X
 
         Args:
-            transfer (jnp.ndarray): match-specific transfers
-            utilities (jnp.ndarray): match-specific utilities for agents of type X
+            transfer (Array): match-specific transfers
+            utilities (Array): match-specific utilities for agents of type X
 
         Returns:
-            ChoiceProbabilities (jnp.ndarray): match-specific choice probabilities for agents of type X
+            ChoiceProbabilities (Array): match-specific choice probabilities for agents of type X
         """
-        v_X = utility_X + transfer
+        v_X = jax.lax.add(utility_X, transfer)
         return self.ChoiceProbabilities(v_X, axis=1)
 
     def ChoiceProbabilities_Y(
-        self, transfer: jnp.ndarray, utility_Y: jnp.ndarray
+        self, transfer: Array, utility_Y: Array
     ) -> tuple[jnp.ndarray, jnp.ndarray]:
         """Computes choice probabilities of agents of type Y
 
         Args:
-            transfer (jnp.ndarray): match-specific transfers
-            utilities (jnp.ndarray): match-specific utilities for agents of type Y
+            transfer (Array): match-specific transfers
+            utilities (Array): match-specific utilities for agents of type Y
 
         Returns:
-            ChoiceProbabilities (jnp.ndarray): match-specific choice probabilities for agents of type Y
+            ChoiceProbabilities (Array): match-specific choice probabilities for agents of type Y
         """
-        v_Y = utility_Y - transfer
+        v_Y = jax.lax.sub(utility_Y, transfer)
         return self.ChoiceProbabilities(v_Y, axis=0)
 
-    def Demand_X(self, transfer: jnp.ndarray, utility_X: jnp.ndarray) -> jnp.ndarray:
+    def Demand_X(self, transfer: Array, utility_X: Array) -> Array:
         """Computes agents of type X's demand for agents of type Y
 
         Args:
-            transfer (jnp.ndarray): match-specific transfers
-            utility_X (jnp.ndarray): match-specific utilities
+            transfer (Array): match-specific transfers
+            utility_X (Array): match-specific utilities
 
         Returns:
-            demand (jnp.ndarray): demand for inside options
+            demand (Array): demand for inside options
         """
         return (
             self.marginal_distribution_X
             * self.ChoiceProbabilities_X(transfer, utility_X)[0]
         )
 
-    def Demand_Y(self, transfer: jnp.ndarray, utility_Y: jnp.ndarray) -> jnp.ndarray:
+    def Demand_Y(self, transfer: Array, utility_Y: Array) -> Array:
         """Computes agents of type Y's demand for agents of type X
 
         Args:
-            transfer (jnp.ndarray): match-specific transfers
-            utility_Y (jnp.ndarray): match-specific utilities
+            transfer (Array): match-specific transfers
+            utility_Y (Array): match-specific utilities
 
         Returns:
-            demand (jnp.ndarray): demand for inside options
+            demand (Array): demand for inside options
         """
         return (
             self.marginal_distribution_Y
@@ -151,19 +149,19 @@ class MatchingModel(Pytree, mutable=False):
 
     def UpdateTransfers(
         self,
-        t_initial: jnp.ndarray,
-        utility_X: jnp.ndarray,
-        utility_Y: jnp.ndarray,
-    ) -> jnp.ndarray:
+        t_initial: Array,
+        utility_X: Array,
+        utility_Y: Array,
+    ) -> Array:
         """Updates fixed point equation for transfers
 
         Args:
-            t_initial (jnp.ndarray): initial transfers
-            utility_X (jnp.ndarray): utility of agents of type X
-            utility_Y (jnp.ndarray): utility of agents of type Y
+            t_initial (Array): initial transfers
+            utility_X (Array): utility of agents of type X
+            utility_Y (Array): utility of agents of type Y
 
         Returns:
-            t_updated (jnp.ndarray): updated transfers
+            t_updated (Array): updated transfers
         """
         # Calculate demand for both sides of the market
         demand_X = self.Demand_X(t_initial, utility_X)  # type X's demand for type Y
@@ -175,24 +173,25 @@ class MatchingModel(Pytree, mutable=False):
 
     def solve(
         self,
-        utility_X: jnp.ndarray,
-        utility_Y: jnp.ndarray,
+        utility_X: Array,
+        utility_Y: Array,
         fixed_point_solver: SolverTypes = SquaremAcceleration,
         tol: float = 1e-10,
         maxiter: int = 1000,
         verbose: bool = False,
-    ) -> jnp.ndarray:
+    ) -> Array:
         """Solve for equilibrium transfer
 
         Args:
-            params (jnp.ndarray): structural parameters
+            utility_X (Array): utilities of agents of type X
+            utility_Y (Array): utilities of agents of type Y
             fixed_point_solver (SolverTypes): solver used for solving fixed point equation (FixedPointIteration, AndersonAcceleration, SquaremAcceleration)
             tol (float): stopping tolerance for step length of fixed-point iterations, x_{i+1} - x_{i}
             maxiter (int): maximum number of iterations
             verbose (bool): whether to print information on every iteration or not.
 
         Returns:
-            transfers (jnp.ndarray): equilibrium transfers
+            transfers (Array): equilibrium transfers
         """
         # Initial guess for equilibrium transfers
         transfer_init = jnp.zeros(self.covariates_X.shape[:-1])
@@ -206,18 +205,16 @@ class MatchingModel(Pytree, mutable=False):
         ).run(transfer_init, utility_X, utility_Y)
         return result.params
 
-    def Utilities_of_agents(
-        self, params: jnp.ndarray
-    ) -> tuple[jnp.ndarray, jnp.ndarray]:
+    def Utilities_of_agents(self, params: Array) -> tuple[Array, Array]:
         """Compute match-specific utilities for agents of type X and Y
 
         Args:
-            params (jnp.ndarray): parameters of agents' utility functions
+            params (Array): parameters of agents' utility functions
 
         Returns:
-        utility_X (jnp.ndarray):
+        utility_X (Array):
             utilities for agents of type X
-        utility_Y (jnp.ndarray):
+        utility_Y (Array):
             utilities for agents of type Y
         """
         number_of_covariates_X = self.covariates_X.shape[-1]
@@ -229,17 +226,15 @@ class MatchingModel(Pytree, mutable=False):
         utility_Y = self.Utility(self.covariates_Y, parameters_Y)
         return utility_X, utility_Y
 
-    def neg_log_likelihood(
-        self, params: jnp.ndarray, data: ObservedData
-    ) -> jnp.ndarray:
+    def neg_log_likelihood(self, params: Array, data: ObservedData) -> Array:
         """Computes the negative log-likelihood function
 
         Args:
-            params (jnp.ndarray): parameters of agents' utility functions
+            params (Array): parameters of agents' utility functions
             data (ObservedData): observed transfers and numbers of matched and unmatched agents
 
         Returns:
-            neg_log_lik (jnp.ndarray): negative log-likelihood value
+            neg_log_lik (Array): negative log-likelihood value
         """
         utility_X, utility_Y = self.Utilities_of_agents(params)
 
@@ -276,23 +271,23 @@ class MatchingModel(Pytree, mutable=False):
 
     def fit(
         self,
-        guess: jnp.ndarray,
+        guess: Array,
         data: ObservedData,
         tol: float = 1e-8,
         maxiter: int = 100,
         verbose: bool | int = True,
-    ) -> jnp.ndarray:
+    ) -> Array:
         """Estimate parameters of matching model by maximum likelihood (minimize the negative log-likelihood function)
 
         Args:
-            guess (jnp.ndarray): initial parameter guess
+            guess (Array): initial parameter guess
             data (ObservedData): observed transfers and numbers of matched and unmatched agents
             tol (float): tolerance of the stopping criterion
             maxiter (int): maximum number of proximal gradient descent iterations
             verbose (bool): if set to True or 1 prints the information at each step of the solver, if set to 2, print also the information of the linesearch
 
         Returns:
-            params (jnp.ndarray): parameter estimates
+            params (Array): parameter estimates
         """
 
         result = LBFGS(
